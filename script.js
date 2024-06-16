3693996d6fd02abb6b1da04b0112ee57bf5522f7
Bhavesh Wagh <bhaveshbm2711@gmail.com>
2024-06-16 13:29:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstWord(s) {
   // your code here
+	if (s === "") {
+    return "";
+  }
+  let str = "";
+  for (let i of s) {
+    if (i === " ") {
+      break;
+    } else {
+      str += i;
+    }
+  }
+  return str;
+	
 }
 
 // Do not change the code below
